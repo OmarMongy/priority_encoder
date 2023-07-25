7cb61746262735505ba6a4c95f4b33a6f3000c6b
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2023-07-25 15:20:42 +0300

    Update priority_encoder.v

--- Source/priority_encoder.v
+++ Source/priority_encoder.v
@@ -1,20 +1,20 @@
 module priority_encoder 
 #(parameter N = 4)
 (
-input [N-1:0] D,
-output reg [$clog2(N) -1 :0] Y,
-output V
+input [N-1:0] w,
+output reg [$clog2(N) -1 :0] y,
+output z
 );
 integer k;
-assign V = |D;
+assign z = |w;
 
-    always @(D)
+always @(w)
 
 begin
-    Y = 'bx;
+    y = 'bx;
 for(k = 0; k < N; k = k + 1)
-    if (D[k])
-   Y = k;
+   if (w[k])
+   y = k;
 end
 
 endmodule
